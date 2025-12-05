f47bb60216628638b039f9c4235e900a66fa717a
Martin Kadlecik <m_kadlecik@utb.cz>
2025-12-05 13:37:17 +0100

    Add search logic to tab1 using weatherservice

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { WeatherService } from '../services/weather.service';
 
 @Component({
   selector: 'app-tab1',
@@ -9,5 +10,35 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
 export class Tab1Page {
-  constructor() {}
+  city: string = '';
+  loading = false;
+  error: string | null = null;
+  currentWeather: any = null;
+
+  constructor(private weatherService: WeatherService) {}
+
+  searchCity() {
+    this.error = null;
+
+    if (!this.city || this.city.trim().length === 0) {
+      this.error = 'Zadej název města.';
+      return;
+    }
+
+    this.loading = true;
+    this.currentWeather = null;
+
+    this.weatherService.getCurrentWeather(this.city.trim())
+      .subscribe({
+        next: (data) => {
+          this.currentWeather = data;
+          this.loading = false;
+        },
+        error: (err) => {
+          console.error(err);
+          this.error = 'Nepodařilo se načíst počasí. Zkontroluj název města nebo připojení.';
+          this.loading = false;
+        }
+      });
+  }
 }

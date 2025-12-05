f06203e782420ecbf6c9ce3f9ea37dbf764e315f
Martin Kadlecik <m_kadlecik@utb.cz>
2025-12-05 16:12:22 +0100

    Add basic UI for tab1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonButton, IonText, IonSpinner, IonCardHeader, IonCard, IonCardTitle, IonCardSubtitle, IonCardContent, IonInput } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { WeatherService } from '../services/weather.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  standalone: true,
+  imports: [IonInput, IonSpinner, IonText, IonButton, IonLabel, IonItem, IonHeader, IonToolbar, IonTitle, IonContent, FormsModule, CommonModule, IonCardContent, IonCardSubtitle, IonCard, IonCardHeader, IonCardTitle],
 })
 export class Tab1Page {
+
   city: string = '';
   loading = false;
   error: string | null = null;
@@ -17,7 +22,15 @@ export class Tab1Page {
 
   constructor(private weatherService: WeatherService) {}
 
+  onCityInput(event: any) {
+    const value = event.detail?.value ?? '';
+    this.city = value;
+    console.log('onCityInput, city =', this.city);
+  }
+
   searchCity() {
+    console.log('searchCity() zavolána, city =', this.city);
+
     this.error = null;
 
     if (!this.city || this.city.trim().length === 0) {
@@ -31,11 +44,12 @@ export class Tab1Page {
     this.weatherService.getCurrentWeather(this.city.trim())
       .subscribe({
         next: (data) => {
+          console.log('API odpověď:', data);
           this.currentWeather = data;
           this.loading = false;
         },
         error: (err) => {
-          console.error(err);
+          console.error('Chyba API:', err);
           this.error = 'Nepodařilo se načíst počasí. Zkontroluj název města nebo připojení.';
           this.loading = false;
         }
